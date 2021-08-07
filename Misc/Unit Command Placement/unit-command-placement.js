1934e1167a4700fec6880bed0ffc569c24f9815f
GK <thegrovekeeper@gmail.com>
2021-08-07 14:41:25 -0400

    Update unit-command-placement.js

--- Misc/Unit Command Placement/unit-command-placement.js
+++ Misc/Unit Command Placement/unit-command-placement.js
@@ -2,7 +2,6 @@
  * Script by Blindcoco
  */
 
-
 UnitCommand.configureCommands = function(groupArray) {
     this._appendTalkEvent(groupArray);
     groupArray.appendObject(UnitCommand.Attack);
@@ -12,6 +11,7 @@ UnitCommand.configureCommands = function(groupArray) {
     groupArray.appendObject(UnitCommand.Village);
     groupArray.appendObject(UnitCommand.Shop);
     groupArray.appendObject(UnitCommand.Gate);
+    // Unit commands used to be here
     groupArray.appendObject(UnitCommand.Quick);
     groupArray.appendObject(UnitCommand.Steal);
     groupArray.appendObject(UnitCommand.Wand);
@@ -19,7 +19,7 @@ UnitCommand.configureCommands = function(groupArray) {
     this._appendMetamorphozeCommand(groupArray);
     this._appendFusionCommand(groupArray);
     groupArray.appendObject(UnitCommand.Item);
-    this._appendUnitEvent(groupArray);
+    this._appendUnitEvent(groupArray); // Unit events are now here
     groupArray.appendObject(UnitCommand.Trade);
     groupArray.appendObject(UnitCommand.Stock);
     groupArray.appendObject(UnitCommand.MetamorphozeCancel);
